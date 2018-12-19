f30aabaa71dca5dbfab4e0385b9cf66bcd65bd6b
Sergey Chepurnenko <ProgrammerKun@gmail.com>
2018-12-19 18:02:53 +0300

    Fixed org open command.

--- bin/sfdx_org_open.cmd
+++ bin/sfdx_org_open.cmd
@@ -3,12 +3,12 @@ CLS
 ECHO SFDX tool - Service - Open Org
 ECHO.
 
-SET /P target=Input username or alias for the target org (or 0 to exit) and press ENTER: 
+SET /P alias=Input username or alias for the target org (or 0 to exit) and press ENTER: 
 IF %alias%==0 GOTO EXIT
 
 ECHO.
 
-CALL sfdx force:org:open -u %target%
+CALL sfdx force:org:open -u %alias%
 
 ECHO.
 PAUSE
