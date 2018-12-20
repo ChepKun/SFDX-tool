f050f9eee7bb861895e38570405086fdec62a645
Sergey Chepurnenko <ProgrammerKun@gmail.com>
2018-12-20 14:33:07 +0300

    Updated package install report script.

--- bin/sfdx_package_install_report.cmd
+++ bin/sfdx_package_install_report.cmd
@@ -8,11 +8,15 @@ IF %alias%==0 GOTO EXIT
 SET /P requestId=Input the ID install request (or 0 to exit) and press ENTER: 
 IF %requestId%==0 GOTO EXIT
 
+:GET_REPORT
 ECHO.
 
 CALL sfdx force:package:install:report -i %requestId% -u %alias%
 
 ECHO.
-PAUSE
+
+SET /P option=Do you want to get report again? (Y/N): 
+IF %option%==Y GOTO GET_REPORT
+IF %option%==y GOTO GET_REPORT
 
 :EXIT
