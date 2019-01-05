7f3b0e8974fbe5adc124b707e062e994cf16e155
Sergey Chepurnenko <ProgrammerKun@gmail.com>
2019-01-05 16:05:09 +0300

    Minor fixes, added a new value to examples.

--- bin/sfdx_org_create.cmd
+++ bin/sfdx_org_create.cmd
@@ -3,7 +3,7 @@ CLS
 ECHO SFDX tool - Service - Create Scratch Org
 ECHO.
 
-SET /P alias=Input an alias for for the created scratch org (or 0 to exit) and press ENTER: 
+SET /P alias=Input an alias for a new scratch org (or 0 to exit) and press ENTER: 
 IF %alias%==0 GOTO EXIT
 SET /P devhub=Input username or alias for the dev hub org (or 0 to exit) and press ENTER: 
 IF %devhub%==0 GOTO EXIT
